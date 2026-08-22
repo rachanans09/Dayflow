97a1e993b34ae0ca43eb0231ab3227ebc01d9c04
Rachana N Shekhar <rachanans2007@gmail.com>
2026-08-22 11:48:16 +0530

    Fix react-router-dom import name

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react_router_dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import SignIn from "./pages/Auth/SignIn";
 import SignUp from "./pages/Auth/SignUp";
 import Dashboard from "./pages/Dashboard";
