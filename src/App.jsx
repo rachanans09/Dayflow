e15ec1debabebea322b1f8256965c2220352a050
Rohith Venkat Raj <rohithvenkatraj.official@gmail.com>
2026-08-22 11:52:49 +0530

    Clean up conflict markers in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,9 @@
-<<<<<<< HEAD
-
-
 import AttendanceDashboard from "./pages/Attendance/attendance";
-=======
-import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import SignIn from "./pages/Auth/SignIn";
-import SignUp from "./pages/Auth/SignUp";
-import Dashboard from "./pages/Dashboard";
-import Profile from "./pages/Profile/Profile";
-import Attendance from "./pages/Attendance/Attendance";
->>>>>>> 59cbfd6e06abea17940ec422d1a4766807885151
 import ApplyLeave from "./pages/Leave/ApplyLeave";
 import ApproveLeave from "./pages/Leave/ApproveLeave";
 
 function App() {
   return (
-<<<<<<< HEAD
     <div style={{ padding: "20px" }}>
       {/* Attendance Module */}
       <section style={{ marginBottom: "40px" }}>
@@ -34,19 +21,6 @@ function App() {
         <ApproveLeave />
       </section>
     </div>
-=======
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<SignIn />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/attendance" element={<Attendance />} />
-        <Route path="/apply-leave" element={<ApplyLeave />} />
-        <Route path="/approve-leave" element={<ApproveLeave />} />
-      </Routes>
-    </BrowserRouter>
->>>>>>> 59cbfd6e06abea17940ec422d1a4766807885151
   );
 }
 
