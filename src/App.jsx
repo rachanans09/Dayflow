67e9ea650611c193835154333e3a2a15fd09de8c
Ritisha Chavan <critisha007@gmail.com>
2026-08-22 11:36:17 +0530

    Integrated Attendance and Leave components in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,26 @@
-<<<<<<< HEAD
 import AttendanceDashboard from "./pages/Attendance/attendance";
-=======
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import SignIn from "./pages/Auth/SignIn";
-import SignUp from "./pages/Auth/SignUp";
-import Dashboard from "./pages/Dashboard";
-import Profile from "./pages/Profile/Profile";
-import Attendance from "./pages/Attendance/Attendance";
 import ApplyLeave from "./pages/Leave/ApplyLeave";
 import ApproveLeave from "./pages/Leave/ApproveLeave";
->>>>>>> 8c0564ae7376a1d05f10fded091d7458877662dc
 
 function App() {
   return (
-<<<<<<< HEAD
-    <div>
-      <AttendanceDashboard />
+    <div style={{ padding: "20px" }}>
+      {/* Attendance Module */}
+      <section style={{ marginBottom: "40px" }}>
+        <AttendanceDashboard />
+      </section>
+
+      <hr style={{ border: "1px solid #334155", margin: "40px 0" }} />
+
+      {/* Member 4: Leave Management Modules */}
+      <section style={{ marginBottom: "40px" }}>
+        <ApplyLeave />
+      </section>
+
+      <section>
+        <ApproveLeave />
+      </section>
     </div>
-=======
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<SignIn />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/attendance" element={<Attendance />} />
-        <Route path="/apply-leave" element={<ApplyLeave />} />
-        <Route path="/approve-leave" element={<ApproveLeave />} />
-      </Routes>
-    </BrowserRouter>
->>>>>>> 8c0564ae7376a1d05f10fded091d7458877662dc
   );
 }
 
